57858d1e6aeae9ca47a80b35a7af5940a3430796
Chanhee Park <chanheepark@Chanheeui-MacBookPro.local>
2021-06-08 15:08:24 +0900

    feat: remove agumentator histogram

--- js/iap/iap.jsx
+++ js/iap/iap.jsx
@@ -8,71 +8,19 @@ function IAP({
 }) {
   return (
     <div className="block block__iap">
-      <div className="component-group iap__component-group--left">
-        <PCP
-          instances={instances}
-          features={features}
-          selectedInstances={selectedInstances}
-          focusedInstance={focusedInstance}
-          setSelectedInstances={setSelectedInstances}
-        />
-        <Scatter
-          instances={instances}
-          features={features}
-          selectedInstances={selectedInstances}
-          focusedInstance={focusedInstance}
-        />
-      </div>
-      <div className="component-group iap__component-group--right">
-        <Options />
-        <Augmentator features={features} />
-      </div>
+      <PCP
+        instances={instances}
+        features={features}
+        selectedInstances={selectedInstances}
+        focusedInstance={focusedInstance}
+        setSelectedInstances={setSelectedInstances}
+      />
+      <Scatter
+        instances={instances}
+        features={features}
+        selectedInstances={selectedInstances}
+        focusedInstance={focusedInstance}
+      />
     </div>
   );
-}
-
-function Options() {
-  return <div className="component component__option"> </div>;
-}
-
-class Augmentator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedFeatures: Object.keys(this.props.features),
-    };
-  }
-
-  popFeature(featureName) {
-    const copy = this.state.selectedFeatures;
-    const idx = copy.indexOf(featureName);
-    if (idx > -1) copy.splice(idx, 1);
-    this.setState({ selectedFeatures: copy });
-  }
-
-  pushFeature(featureName) {
-    const copy = this.state.selectedFeatures;
-    copy.push(featureName);
-    this.setState({ selectedFeatures: copy });
-  }
-
-  render() {
-    return (
-      <div className="component component__agumentator">
-        <div className="agumentator__histograms">
-          {/* <div className="histogram-add-button__wrapper">
-            <Button text={"add"} onClick={() => {}} />
-          </div> */}
-          {this.state.selectedFeatures.map((fName) => (
-            <div key={`histogram-${fName}`}>
-              <HistogramFilter
-                feature={this.props.features[fName]}
-                onRemove={() => this.popFeature(fName)}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  }
 }
