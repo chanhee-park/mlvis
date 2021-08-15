b51c35913faf2696c589d3ed2660b94ed8a310e7
Chanhee Park <chanheepark@Chanheeui-MacBookPro.local>
2021-08-15 15:22:05 +0900

    more readable debug log

--- js/index.jsx
+++ js/index.jsx
@@ -7,15 +7,17 @@ window.onload = async () => {
   const features = Data.getFeatureInfo(instances);
   const augmentations = Data.augmentateInstances(instances, features, dataname);
 
-  console.log("features:", features);
-  console.log("instances:", instances);
-  console.log("instance:", instances[0]);
-  console.log("augmentations:", augmentations);
-  console.log("augmentation:", augmentations[0]);
+  console.log("features:", { features, first: Object.values(features)[0] });
+  console.log("instances:", { instances, first: instances[0] });
+  console.log("augmentations:", { augmentations, first: augmentations[0] });
   console.log("Data is fully loaded.");
 
   ReactDOM.render(
-    <App instances={instances} features={features} augmentations={augmentations} />,
+    <App
+      instances={instances}
+      features={features}
+      augmentations={augmentations}
+    />,
     document.getElementById("root")
   );
 };
